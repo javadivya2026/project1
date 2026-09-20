119949f0546a614b3d2db17aea4eeaf7205aa9bd
javadivya2026 <javadivya@outlook.com>
2026-09-20 19:14:08 +0530

    Add boolean variables and print statements in AppUtil
    Added boolean variables and print statements to check their values.

--- src/main/java/com/example/AppUtil.java
+++ src/main/java/com/example/AppUtil.java
@@ -1,4 +1,10 @@
 package com.example;
 
 public class AppUtil {
+
+    boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+
 }
